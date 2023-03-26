7b68475b552f92ef108af013c229305d7981bc64
EdgeJay <hjwu85@gmail.com>
2023-03-26 22:50:56 +0800

    add env method to return bot token

--- telegram/env/env.go
+++ telegram/env/env.go
@@ -1,8 +1,11 @@
 package env
 
 import (
+	"errors"
 	"fmt"
 	"os"
+
+	awsUtils "github.com/EdgeJay/lifecyclebot/utils/aws"
 )
 
 func GetAWSParamStoreKeyName(name string) string {
@@ -19,4 +22,17 @@ func GetAppEnv() string {
 
 func GetAppVersion() string {
 	return os.Getenv("app_version")
+}
+
+func GetTelegramBotToken() (string, error) {
+	token := awsUtils.GetStringParameter(
+		GetAWSParamStoreKeyName("telegram_bot_token"),
+		"",
+	)
+
+	if token != "" {
+		return token, nil
+	} else {
+		return "", errors.New("invalid Telegram bot token")
+	}
 }
